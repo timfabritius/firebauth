e13ffbc798e6ff77c598d7c2b8a5992ad4d8c3a8
timfabritius <tc.fabritius@gmail.com>
2018-05-04 00:41:35 +0300

    V1

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY =  "AIzaSyDrgvO6aTlB-acuLcEJkInG4985eu_h0fI";
+export const FIREBASE_AUTH_DOMAIN = "localhost";
+export const FIREBASE_DATABASE_URL = "https://react-test-df635.firebaseio.com/";
+export const FIREBASE_PROJECT_ID = " react-test-df635 ";
+export const FIREBASE_STORAGE_BUCKET = "gs://react-test-df635.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "1053981679269";
+
+export const FACEBOOK_APP_ID = "495651567503592";
